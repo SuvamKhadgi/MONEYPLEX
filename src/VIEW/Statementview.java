05cdbf8bd1c2622278365160a6afd8b0b828582b
suvam khadgi <appleb986@gmail.com>
2023-06-30 17:56:31 +0545

    printing doing

---
+++ src/VIEW/Statementview.java
@@ -0,0 +1,190 @@
+package VIEW;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+
+public class Statementview extends javax.swing.JFrame {
+
+    public Statementview() {
+        initComponents();
+        setExtendedState(NORMAL);
+        ImageIcon myimage = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("Moneyplex Bank.png")));
+        Image img1 = myimage.getImage();
+        Image img2 = img1.getScaledInstance(jLabel1.getWidth(), jLabel1.getHeight(), Image.SCALE_SMOOTH);
+        ImageIcon i = new ImageIcon(img2);
+        jLabel1.setIcon(i);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btnhome = new javax.swing.JButton();
+        btncustomermg = new javax.swing.JButton();
+        btntransaction = new javax.swing.JButton();
+        btntransachistory = new javax.swing.JButton();
+        btnintrest = new javax.swing.JButton();
+        btnloan = new javax.swing.JButton();
+        btnreport = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtmybill = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("MONEYPLEX");
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnhome.setBackground(new java.awt.Color(42, 144, 174));
+        btnhome.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
+        btnhome.setText("DASHBOARD");
+        btnhome.setBorderPainted(false);
+        btnhome.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnhomeActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnhome, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 130, 150, 50));
+
+        btncustomermg.setBackground(new java.awt.Color(42, 144, 174));
+        btncustomermg.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
+        btncustomermg.setText("CUSTOMERS");
+        btncustomermg.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btncustomermgActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btncustomermg, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 130, 150, 50));
+
+        btntransaction.setBackground(new java.awt.Color(42, 144, 174));
+        btntransaction.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
+        btntransaction.setText("TRANSACTION");
+        btntransaction.setHorizontalTextPosition(javax.swing.SwingConstants.LEADING);
+        btntransaction.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btntransactionActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btntransaction, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 130, 180, 50));
+
+        btntransachistory.setBackground(new java.awt.Color(0, 255, 255));
+        btntransachistory.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
+        btntransachistory.setText("STATEMENT");
+        getContentPane().add(btntransachistory, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 130, 150, 50));
+
+        btnintrest.setBackground(new java.awt.Color(42, 144, 174));
+        btnintrest.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
+        btnintrest.setText("INTEREST");
+        btnintrest.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnintrestActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnintrest, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 130, 150, 50));
+
+        btnloan.setBackground(new java.awt.Color(42, 144, 174));
+        btnloan.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
+        btnloan.setText("LOAN");
+        getContentPane().add(btnloan, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 130, 150, 50));
+
+        btnreport.setBackground(new java.awt.Color(42, 144, 174));
+        btnreport.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
+        btnreport.setText("MORE...");
+        getContentPane().add(btnreport, new org.netbeans.lib.awtextra.AbsoluteConstraints(930, 130, 150, 50));
+
+        txtmybill.setColumns(20);
+        txtmybill.setRows(5);
+        jScrollPane1.setViewportView(txtmybill);
+
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 230, 1080, 360));
+
+        jButton1.setText("PRINT ME");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(415, 600, 150, 30));
+
+        jLabel2.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
+        jLabel2.setText("ACCOUNT NO");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 200, -1, 30));
+        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 200, 130, 30));
+
+        jButton2.setText("SEARCH");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 200, 90, 30));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1080, 640));
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btntransactionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btntransactionActionPerformed
+        this.setVisible(false);
+        Withdrawview ca= new Withdrawview();
+        ca.setVisible(true);
+    }//GEN-LAST:event_btntransactionActionPerformed
+
+    private void btnhomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnhomeActionPerformed
+
+    }//GEN-LAST:event_btnhomeActionPerformed
+
+    private void btncustomermgActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btncustomermgActionPerformed
+        this.setVisible(false);
+        CreateAccountview ca= new CreateAccountview();
+        ca.setVisible(true);
+    }//GEN-LAST:event_btncustomermgActionPerformed
+
+    private void btnintrestActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnintrestActionPerformed
+       this.setVisible(false);
+       Interestview ca= new Interestview();
+       ca.setVisible(true); 
+    }//GEN-LAST:event_btnintrestActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       try{
+           txtmybill.print();
+       }catch(Exception e){
+           
+       }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        txtmybill.setText("\tMONEYPLEX BANK"+"\n"+"\tDILLIBAZAR,KATHMANDU"+"\n"+ "\tPHONE NO: 9841000000" +"\n"+ "*******************************************************-+");
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Statementview().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btncustomermg;
+    private javax.swing.JButton btnhome;
+    private javax.swing.JButton btnintrest;
+    private javax.swing.JButton btnloan;
+    private javax.swing.JButton btnreport;
+    private javax.swing.JButton btntransachistory;
+    private javax.swing.JButton btntransaction;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextArea txtmybill;
+    // End of variables declaration//GEN-END:variables
+}
