d96706453b06d521a0798bf86bb85a32668dbaad
suvam khadgi <appleb986@gmail.com>
2023-07-07 11:59:59 +0545

    register done

--- src/CONTROLLER/NewAdmincontroller.java
+++ src/CONTROLLER/NewAdmincontroller.java
@@ -1,80 +1,123 @@
+//package CONTROLLER;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import MODEL.*;
+//import VIEW.*;
+//import java.sql.*;
+//import javax.swing.JOptionPane;
+//public class NewAdmincontroller {
+//NewAdminmodel mymodel1;
+//NewAdminview myview1;
+//ResultSet rs;
+//PreparedStatement pst=null;
+//Statement stmt;
+//
+//public NewAdmincontroller(NewAdminview myView1)
+//{
+//    this.myview1=myView1;
+//            myview1.addLoginListner(new RegisterListener());
+//}
+//class RegisterListener implements ActionListener
+//{
+//    @Override
+//    public void actionPerformed(ActionEvent e) 
+//    {
+//        try
+//        {
+//        mymodel1=myview1.getMymodel();
+//        if(checkMyData(mymodel1))
+//        {
+//        myview1.showMessage("Registered Successfully");
+//        myview1.resetme();
+//                myview1.addLoginListner(this);
+//        }
+//        else
+//        {
+//        myview1.showMessage("Invalid username or password");
+//        }
+//        }
+//        catch(Exception e1)
+//        {
+//            
+//        }
+//    }
+//}
+//public boolean checkMyData(NewAdminmodel mymodel1) {  
+//    Connection conn = null;
+//    try {
+//        conn = DataConnection.dbconnect();
+//        String sql = "insert into e_info(emp_fname, emp_lname, emp_phone, emp_email, emp_password) values(?,?,?,?,?)";
+//        pst = conn.prepareStatement(sql);
+//        pst.setString(1, mymodel1.getFname());
+//        pst.setString(2, mymodel1.getLname());
+//        pst.setString(3, mymodel1.getPhone());
+//        pst.setString(4, mymodel1.getEmail());
+//        pst.setString(5, mymodel1.getPassword());
+//        pst.executeUpdate();
+//        System.out.println("Data inserted");
+//        return true;
+//    } catch (Exception e2) {
+//        e2.printStackTrace();
+//    } finally {
+//        if (pst != null) {
+//            try {
+//                pst.close();
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        if (conn != null) {
+//            try {
+//                conn.close();
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//    return false;
+//}
+//}
+//
+
 package CONTROLLER;
+
+import MODEL.NewAdminmodel;
+import VIEW.NewAdminview;
+import DAO.NewAdminDAO;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import MODEL.*;
-import VIEW.*;
-import java.sql.*;
-import javax.swing.JOptionPane;
+
 public class NewAdmincontroller {
-NewAdminmodel mymodel1;
-NewAdminview myview1;
-ResultSet rs;
-PreparedStatement pst=null;
-Statement stmt;
+    private NewAdminmodel mymodel1;
+    private NewAdminview myview1;
+    private NewAdminDAO newAdminDAO;
 
-public NewAdmincontroller(NewAdminview myView1)
-{
-    this.myview1=myView1;
-            myview1.addLoginListner(new RegisterListener());
-}
-class RegisterListener implements ActionListener
-{
-    @Override
-    public void actionPerformed(ActionEvent e) 
-    {
-        try
-        {
-        mymodel1=myview1.getMymodel();
-        if(checkMyData(mymodel1))
-        {
-        myview1.showMessage("Registered Successfully");
-        myview1.resetme();
-                myview1.addLoginListner(this);
-        }
-        else
-        {
-        myview1.showMessage("Invalid username or password");
-        }
-        }
-        catch(Exception e1)
-        {
-            
-        }
+    public NewAdmincontroller(NewAdminview myView1) {
+        this.myview1 = myView1;
+        this.newAdminDAO = new NewAdminDAO();
+        myview1.addLoginListner(new RegisterListener());
     }
-}
-public boolean checkMyData(NewAdminmodel mymodel1) {  
-    Connection conn = null;
-    try {
-        conn = DataConnection.dbconnect();
-        String sql = "insert into e_info(emp_fname, emp_lname, emp_phone, emp_email, emp_password) values(?,?,?,?,?)";
-        pst = conn.prepareStatement(sql);
-        pst.setString(1, mymodel1.getFname());
-        pst.setString(2, mymodel1.getLname());
-        pst.setString(3, mymodel1.getPhone());
-        pst.setString(4, mymodel1.getEmail());
-        pst.setString(5, mymodel1.getPassword());
-        pst.executeUpdate();
-        System.out.println("Data inserted");
-        return true;
-    } catch (Exception e2) {
-        e2.printStackTrace();
-    } finally {
-        if (pst != null) {
+
+    class RegisterListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
             try {
-                pst.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        if (conn != null) {
-            try {
-                conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
+                mymodel1 = myview1.getMymodel();
+                if (checkMyData(mymodel1)) {
+                    myview1.showMessage("Registered Successfully");
+                    myview1.resetme();
+                    myview1.addLoginListner(this);
+                } else {
+                    myview1.showMessage("Invalid username or password");
+                }
+            } catch (Exception e1) {
+                e1.printStackTrace();
             }
         }
     }
-    return false;
-}
-}
 
+    public boolean checkMyData(NewAdminmodel mymodel1) {
+        return newAdminDAO.checkMyData(mymodel1);
+    }
+}
--- src/DAO/NewAdminDAO.java
+++ src/DAO/NewAdminDAO.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
 
-/**
- *
- * @author LEGION
- */
+import MODEL.DataConnection;
+import MODEL.NewAdminmodel;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class NewAdminDAO {
-    
+    public boolean checkMyData(NewAdminmodel mymodel1) {
+        Connection conn = null;
+        PreparedStatement pst = null;
+
+        try {
+            conn = DataConnection.dbconnect();
+            String sql = "INSERT INTO e_info(emp_fname, emp_lname, emp_phone, emp_email, emp_password) VALUES (?, ?, ?, ?, ?)";
+            pst = conn.prepareStatement(sql);
+            pst.setString(1, mymodel1.getFname());
+            pst.setString(2, mymodel1.getLname());
+            pst.setString(3, mymodel1.getPhone());
+            pst.setString(4, mymodel1.getEmail());
+            pst.setString(5, mymodel1.getPassword());
+            pst.executeUpdate();
+            System.out.println("Data inserted");
+            return true;
+        } catch (Exception e2) {
+            e2.printStackTrace();
+        } finally {
+            if (pst != null) {
+                try {
+                    pst.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
 }
